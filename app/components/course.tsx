5514255b50d30fc597a1865def50647d134e3f4b
DANCAN KARANI <karanidancan120@gmail.com>
2024-08-21 10:01:23 +0300

    my chnages

--- app/components/course.tsx
+++ app/components/course.tsx
@@ -39,46 +39,55 @@ const Job: React.FC<CourseProps> = ({ Level:level, CourseName:course_name, KcseG
     <div className="job-item">Mechanical Engineering</div>
     <div className="job-item">C+</div>
 </div>
+<br />
 <div className="flex p-20px text-20">
     <div className="job-item">Diploma</div>
     <div className="job-item">Electrical Engineering</div>
     <div className="job-item">C</div>
 </div>
+<br />
 <div className="flex p-20px text-20">
     <div className="job-item">Advanced Diploma</div>
     <div className="job-item">Civil Engineering</div>
     <div className="job-item">B-</div>
 </div>
+<br />
 <div className="flex p-20px text-20">
     <div className="job-item">Diploma</div>
     <div className="job-item">Automotive Engineering</div>
     <div className="job-item">C</div>
 </div>
+<br />
 <div className="flex p-20px text-20">
     <div className="job-item">Certificate</div>
     <div className="job-item">Plumbing</div>
     <div className="job-item">D+</div>
 </div>
+<br />
 <div className="flex p-20px text-20">
     <div className="job-item">Diploma</div>
     <div className="job-item">Information Technology</div>
     <div className="job-item">C</div>
 </div>
+<br />
 <div className="flex p-20px text-20">
     <div className="job-item">Advanced Diploma</div>
     <div className="job-item">Telecommunication Engineering</div>
     <div className="job-item">B</div>
 </div>
+<br />
 <div className="flex p-20px text-20">
     <div className="job-item">Certificate</div>
     <div className="job-item">Carpentry and Joinery</div>
     <div className="job-item">D+</div>
 </div>
+<br />
 <div className="flex p-20px text-20">
     <div className="job-item">Diploma</div>
     <div className="job-item">Building and Construction</div>
     <div className="job-item">C-</div>
 </div>
+<br />
 <div className="flex p-20px text-20">
     <div className="job-item">Certificate</div>
     <div className="job-item">Culinary Arts</div>
